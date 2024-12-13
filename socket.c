fdc9c471a9e1d47067c125c92ac83f0743c94696
Gökdeniz Dağ <mountainskysea07@gmail.com>
2024-12-13 22:26:45 +0200

    editeed some functions into more convetionel ways of c programming

--- socket.c
+++ socket.c
@@ -2,27 +2,28 @@
 void PrintLastError(const char* msg) {
     printf("%s: %d\n", msg, WSAGetLastError());
 }
-SOCKET createSocket(int family, int type, int protocol){
-    SOCKET s = socket(family, type, protocol);
+void createSocket(int family, int type, int protocol,SOCKET *s){
+    SOCKET sock = socket(family, type, protocol);
     if(s==INVALID_SOCKET){
         PrintLastError("Error Creating Socket");
         return 1;
     }
-    return s;
+    *s=sock;
 }
 int bindSocket(SOCKET s, char* ip, int port){
-    struct sockaddr_in server = createServer(ip, port);
+    struct sockaddr_in server;
+    createServer(ip, port,&server);
     if(bind(s,(struct sockaddr*) &server,sizeof(server))== SOCKET_ERROR){
         PrintLastError("Error binding socket");
     }
     return 0;
 }
-struct sockaddr_in createServer(char* ip, int port){
+void createServer(char* ip, int port,struct sockaddr_in *serv){
     struct sockaddr_in server;
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = inet_addr(ip);
     server.sin_port = htons(port);
-    return server;
+    *serv=server;
 }
 int listenSocket(SOCKET s,int backlog){
     if(listen(s,backlog)){
@@ -31,13 +32,13 @@ int listenSocket(SOCKET s,int backlog){
     }
     return 0;
 }
-struct Conn acceptConnection(SOCKET s){
+void acceptConnection(SOCKET s,struct Conn *con){
     struct Conn c;
     c.connSocket = accept(s,(struct sockaddr*)&c.server,sizeof(&c.server));
     if(c.connSocket == INVALID_SOCKET){
         PrintLastError("Error Accepting");
     }
-    return c;
+    *con=c;
 }
 void destroySocket(SOCKET s){
     closesocket(s);
--- socket.h
+++ socket.h
@@ -7,9 +7,9 @@ struct Conn{
     struct sockaddr_in server;
     SOCKET connSocket;
 };
-SOCKET createSocket(int family,int type,int protocol);
+void createSocket(int family,int type,int protocol,SOCKET *s);
 int bindSocket(SOCKET s, char* ip, int port);
-struct sockaddr_in createServer(char* ip, int port);
+void createServer(char* ip, int port,struct sockaddr_in *serv);
 void PrintLastError(const char* msg);
 void destroySocket(SOCKET s);
 #endif //ENDIF
